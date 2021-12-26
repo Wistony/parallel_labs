ce813659587ff196734c0568151054813ca633f7
Wistony <42906954+Wistony@users.noreply.github.com>
2021-12-26 16:12:04 +0200

    task 3 added

---
+++ src/Lab1_3/MichaelAndScottAlgorithm.java
@@ -0,0 +1,71 @@
+package Lab1_3;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicReference;
+
+//wiki implementation
+public class MichaelAndScottAlgorithm<T>
+{
+    private Node<T> dummy = new Node<>(null, new AtomicReference<>(null));
+    private AtomicReference<Node<T>> head = new AtomicReference<>(dummy);
+    private AtomicReference<Node<T>> tail = new AtomicReference<>(dummy);
+
+    public T pop()
+    {
+        while (true)
+        {
+            Node<T> currentHead = head.get();
+            Node<T> currentTail = tail.get();
+            Node<T> nextHead = currentHead.next.get();
+
+            if (currentHead == currentTail)
+            {
+                if (nextHead == null)
+                {
+                    throw new NoSuchElementException();
+                }
+                else
+                {
+                    tail.compareAndSet(currentTail, currentTail.next.get());
+                }
+            }
+            else
+            {
+                if (head.compareAndSet(currentHead, nextHead))
+                {
+                    return nextHead.data;
+                }
+            }
+        }
+    }
+
+    public void push(T data)
+    {
+        Node<T> newTail = new Node<>(data, new AtomicReference<>(null));
+
+        while(true)
+        {
+            Node<T> currentTail = tail.get();
+
+            if (currentTail.next.compareAndSet(null, newTail))
+            {
+                tail.compareAndSet(currentTail, newTail);
+                return;
+            }
+            else
+            {
+                tail.compareAndSet(currentTail, currentTail.next.get());
+            }
+        }
+    }
+
+    public void Print() {
+        Node<T> current = head.get();
+
+        while (current != null)
+        {
+            System.out.println(current.data);
+            current = current.next.get();
+        }
+    }
+}
---
+++ src/Lab1_3/Task3.java
@@ -0,0 +1,43 @@
+package Lab1_3;
+
+public class Task3 {
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        MichaelAndScottAlgorithm<String> queue = new MichaelAndScottAlgorithm<>();
+        Thread[] arr = new Thread[50];
+
+        for (int i = 0; i < 50; i++)
+        {
+            arr[i] = new Thread(new QueueTest(queue));
+            arr[i].start();
+        }
+
+        for (int i = 0; i < 50; i++)
+        {
+            arr[i].join();
+        }
+
+        queue.Print();
+    }
+
+    static class QueueTest implements Runnable
+    {
+        private MichaelAndScottAlgorithm<String> queue;
+
+        public QueueTest(MichaelAndScottAlgorithm<String> michaelAndScottAlgorithm)
+        {
+            this.queue = michaelAndScottAlgorithm;
+        }
+
+        @Override
+        public void run()
+        {
+            queue.push(Thread.currentThread().getName());
+            if (Math.random() > 0.4)
+            {
+                queue.pop();
+            }
+        }
+    }
+}
